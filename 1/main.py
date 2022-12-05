2f708f5aa4a8eed8be9abdea665885716d791c57
Robert Kottelin <robert.kottelin@fresenius-kabi.com>
2022-12-05 12:09:50 +0100

    added rust

--- 1/main.py
+++ 1/main.py
@@ -1,33 +1,14 @@
+with open('input.txt') as f:
+    all_packs = f.read().split('\n')+
 
-# Getting data
-with open('input.txt') as file:
-    data = [i for i in file.read().strip().split("\n")]
+separators, packsums, start_span = [i for i in range(len(all_packs)) if all_packs[i] == ''], [], 0
 
+for end_span in separators: 
+    packsums.append(sum([int(cal) for cal in all_packs[start_span:end_span]])) 
+    start_span = end_span +1
 
-# print(data)
+# Part 01
+print("The largest calorie package is worth " + str(sorted(packsums)[-1]) + " calories")
 
-
-# Traversing every STRING in our DATA
-max = 0
-max2 = 0    # Elf with second greatest calories
-max3 = 0    # Elf with third greatest cals
-count = 0
-for item in data:
-    if item == '':
-        count = 0       # Resetting the count | skipping to next Elf
-    else:
-        num = int(item) # Turning string to integer (so we can add)
-        count += num    # Adding to the count if its a number
-
-    # Finding max values
-    if count > max: 
-        max = count     # Elf with most calories
-    elif count > max2:
-        max2 = count    # Elf with second to most 
-    elif count > max3:
-        max3 = count    # Elf with third to most 
-
-
-# Answers
-print("Answer to part 1:", max)
-print("Answer to part 2:", max+max2+max3)
+# part 02
+print("The largest three calorie packages combined are worth " + str(sum(sorted(packsums)[-3:])) + " calories")
